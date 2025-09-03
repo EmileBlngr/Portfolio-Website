50539f26a5fd2066b0da720aa7f5027ee7460f0a
emile_blngr <emile.boulanger2@gmail.com>
2025-09-03 16:23:08 +0200

    Refacto Title style

--- app/src/components/Title.tsx
+++ app/src/components/Title.tsx
@@ -1,15 +1,34 @@
 import React, { ReactNode } from "react";
 import '../styles/components/title.css'
+import { Box, styled, Theme, Typography } from "@mui/material";
+
+const TitleBox = styled(Box)(({ theme }: { theme: Theme }) => ({
+    width: '40%',
+    [theme.breakpoints.down('md')]: {
+      width: '60%',
+    },
+}));
+
+const Divider = styled(Box)(({ theme }: { theme: Theme }) => ({
+    width: '50%',
+    border: '2px solid rgb(30, 56, 133)'
+}));
+
+const TitleText = styled(Typography)(({ theme }: { theme: Theme }) => ({
+    marginBottom: '5%',
+    flexWrap: 'nowrap',
+}));
 
 interface TitleProps {
     children: ReactNode;
-  }
+}
+
 const Title = ({children}: TitleProps) => {
     return(
-        <div className="title-container">
-            <h1 className="title">{children}</h1>
-            <div className="divider"></div>
-        </div>
+        <TitleBox>
+            <TitleText variant="h1">{children}</TitleText>
+            <Divider/>
+        </TitleBox>
     );
 }
 
